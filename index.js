401823f38ce6d82dfca279862d9cb6f1189dbc16
Giang Le <gianglevan94@gmail.com>
2017-11-01 22:08:39 +0700

    auth with passport

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(require('./configs/keys').mongoDbUri)
 
 require('./routes/auth')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../configs/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -9,8 +21,20 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/oauth/callback/google"
     },
-    (accessToken, refreshToken, profile, cb) => {
-      console.log(accessToken + 'abcd');
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({}).then(existUser => {
+        if (existUser) {
+          done(null, existUser);
+        } else {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => {
+              done(null, user);
+            });
+        }
+      });
     }
   )
 );
