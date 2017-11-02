9af55c9f281b2d6c87f17877e1736ca563972069
Giang Le <gianglevan94@gmail.com>
2017-11-02 22:13:20 +0700

    config prod key

---
+++ configs/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '727130811587-m8otpgllo65nu0872bdffh2ulqjne3ir.apps.googleusercontent.com',
+  googleClientSecret: 'Q08eARqomXUjRdHFSNqwBWTZ',
+  mongoDbUri: 'mongodb://dev:123123@ds237815.mlab.com:37815/emaily',
+  cookieKey: 'sdklfjsalkjfdlkasjdflkasjdfkljaskldfjklasjfdlkasjf',
+}
